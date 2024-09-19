94cf58848e938a0c79dfca192a1e625cbfb8c3c3
Neilmark Ochea <Ocheaneilmark1@gmail.com>
2024-09-19 19:38:46 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,35 @@
-<?php
-header("Location: data:text/html,<script>document.write('<h1>URL spoofing using Data Uri</h1>')</script>");
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title></title>
+    <style>
+        body {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+            margin: 0;
+            font-family: Arial, sans-serif;
+            background-color: #fff; /* White background */
+            color: #000; /* Black text */
+        }
+        .container {
+            text-align: center;
+            padding: 30px;
+        }
+        h1 {
+            font-size: 24px;
+            margin-bottom: 10px;
+        }
+        p {
+            font-size: 16px;
+            color: #000; /* Black text for paragraph */
+        }
+    </style>
+</head>
+<body>
+ <?php header("Location: data:text/html,<script>document.write('<h1>URL spoofing using Data Uri</h1>')</script>");?>
+</body>
+</html>
